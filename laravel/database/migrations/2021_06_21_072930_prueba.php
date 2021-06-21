127b8947ec06062b6c5f022909dbed68d538f4db
Anioah <guilleranioah@hotmail.com>
2021-06-21 02:29:45 -0500

    prueba -.-

--- laravel/database/migrations/2021_06_21_072930_prueba.php
+++ laravel/database/migrations/2021_06_21_072930_prueba.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Test2 extends Migration
+class Prueba extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,7 @@ class Test2 extends Migration
      */
     public function up()
     {
-        // AQUI DEBERIA HACER ALGO 
-        // PERO NO
-        // LO HAGO
-        // PORQUE NO
-        // QUIERO
+        //
     }
 
     /**
