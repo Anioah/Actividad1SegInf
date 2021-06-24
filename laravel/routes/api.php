96eab613588a3622ed1f31b2d1a778b85174c6ea
Anioah <guilleranioah@hotmail.com>
2021-06-24 04:03:01 -0500

    changes

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -14,6 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*
+prueba
+*/
+/*
+prueba
+*/
+
+/*
+prueba
+*/
+/*
+prueba
+*/
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
